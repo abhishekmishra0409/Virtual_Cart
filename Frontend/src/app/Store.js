b8fffca9cd2e1cafeacb792e49b96759237a8b05
Abhishek Mishra <abhishekmishra0409@gmail.com>
2024-10-13 20:44:24 +0530

    Update Store.js

--- Frontend/src/app/Store.js
+++ Frontend/src/app/Store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/User/UserSlice.js";
 import productReducer from "../features/Product/ProductSlice.js";
 import blogReducer from "../features/Blog/BlogSlice.js";
-import contactReducer from "../features/Contact/contactSlice";
+import contactReducer from "../features/Contact/ContactSlice";
 import cartReducer from "../features/cart/CartSlice.js";
 import authMiddleware from "../Middleware/authMiddleware.js";
 
